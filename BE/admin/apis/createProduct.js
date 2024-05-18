a317c263b2a7d8940503b44bbd276545560dfdc9
Rohan Kute <rohanbk309@gmail.com>
2024-05-18 16:14:18 +0530

    img-upload

--- BE/admin/apis/createProduct.js
+++ BE/admin/apis/createProduct.js
@@ -31,13 +31,3 @@ const createProduct = router.post('/create-product', upload.array('file'), async
 });
 
 module.exports = {createProduct};
-// const cloudinary = require('cloudinary');
-
-// cloudinary.v2.config({
-//   cloud_name: process.env.CLOUD_NAME,
-//   api_key: process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_API_SECRET,
-//   secure: true,
-// });
-
-// module.exports = {cloudinary};
