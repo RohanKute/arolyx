90b8d67c3398eed7ebe396aed71063232cd35751
Rohan Kute <rohanbk309@gmail.com>
2024-05-20 19:04:51 +0530

    create-product-admin

--- BE/admin/apis/createProduct.js
+++ BE/admin/apis/createProduct.js
@@ -1,33 +1,52 @@
-// create product  
-
-
-
 const express = require('express');
 const router = express.Router();
-const {PrismaClient, Prisma} = require('@prisma/client');
+const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const multer = require('multer');
-const storage = multer.memoryStorage()
-const upload = multer({ storage })
-const datauri = require('datauri');
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 const DatauriParser = require('datauri/parser');
 const parser = new DatauriParser();
-const path = require('path');
 const { cloudinary } = require('../utils/helpers/cloudinaryConfig');
 
 const createProduct = router.post('/create-product', upload.array('file'), async (req, res) => {
     try {
-             req.files.map(async(image)=>{
-             const buffer = parser.format('.png', image.buffer);
-             const res = await cloudinary.v2.uploader.upload(buffer.content , {folder : "arolyx"});
-             console.log(res);
-           
-        })
+        const imageUrlArr = [];
+        const userData = req.body;
 
+        for (const image of req.files) {
+            const buffer = parser.format('.png', image.buffer);
+            const uploadResult = await cloudinary.v2.uploader.upload(buffer.content, { folder: "arolyx" });
+            if (uploadResult.url) {
+                imageUrlArr.push(uploadResult.url);
+            }
+        }
+        const makeVisibleToUser = (userData.makeVisibleToUser === 'true')
+        if (userData) {
+            const product = await prisma.product.create({
+                data: {
+                    name: userData.name,
+                    description: userData.description,
+                    price: userData.price,
+                    imgUrl: imageUrlArr,
+                    makeVisibleToUser: makeVisibleToUser
+                }
+            });
+            if (product) {
+                console.log(product)
+                return res.status(200).json({
+                    message: "product-added-success",
+                    code: 0,
+                });
+            }
+        }
+        return res.json({
+            message: "input-fields-empty"
+        });
     } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: error.message });
+        console.log(error)
+        res.status(500).json({ error: "internel-server-error" });
     }
 });
 
-module.exports = {createProduct};
+module.exports = { createProduct };
